81b8339bd8259b81422b205a61e21af5134c23a4
Alvaro <alvaro.serrano@wmich.edu>
2020-08-20 11:15:16 +0200

    adding items to the cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import (home, products, checkout)
+from .views import ( checkout, add_to_cart, HomeView, ItemDetailView)
 
 app_name='ecommerce'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('products/', products, name='products'),
+    path('', HomeView.as_view(), name='home'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
+    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart'),
 ]
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item, OrderItem, Order
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 
-def home (request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "ecommerce/home-page.html", context)
+class HomeView(ListView):
+    model = Item
+    template_name= 'ecommerce/home-page.html'
 
-def products(request):
-    context={
-        'items': Item.objects.all()
-    }
-    return render(request, 'ecommerce/product-page.html', context)
-
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'ecommerce/product-page.html'
+  
 def checkout(request):
     context={
         'items': Item.objects.all()
     }
     return render(request, 'ecommerce/checkout-page.html', context)
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date =ordered_date)
+        order.items.add(order_item)
+    return redirect("ecommerce:product", slug=slug)
